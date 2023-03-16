3c7940113bc603282de1ea77408003af22668822
Megha <megha.ahirwar@astoriasolutions.com>
2023-03-16 22:37:01 +0530

    fruit class added with init function

--- classANDobject.py
+++ classANDobject.py
@@ -9,3 +9,13 @@ print(m.name)
 print(m.age)
 print(m.weight)
 print(m.girl)
+
+
+class Fruits:
+  def __init__(self, fruit_name, fruit_color):
+    self.fruit_name = fruit_name
+    self.fruit_color = fruit_color
+
+p1 = Fruits("Apple", "brown")
+print(p1.fruit_name)
+print(p1.fruit_color)
