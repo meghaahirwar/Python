7e292443eefa9470aafff4265c64be60ed3d3102
Megha <megha.ahirwar@astoriasolutions.com>
2023-03-16 22:00:18 +0530

    comments file added

---
+++ comments.py
@@ -0,0 +1,4 @@
+# Comments are used for adding any description about the code and that is ignored by the python interpreter.
+# so in order to use a comments in the python file we need to use # symbol in the beginning of the line.
+# these all are the comments
+# also we can use a multiline comments by using # symbol in each line
