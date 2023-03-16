ffc5d781d2868bed9178c8d1f15df4e651991780
Megha <megha.ahirwar@astoriasolutions.com>
2023-03-17 00:26:51 +0530

    looping string changed

--- strings.py
+++ strings.py
@@ -33,3 +33,12 @@ print("free" in txt)
 txt = "The best things in life are free!"
 if "free" in txt:
   print("Yes, 'free' is present.")
+  
+#Check if NOT
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+#print only if "expensive" is NOT present:
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
